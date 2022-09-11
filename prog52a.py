da59f311b7a3a24be25d774d33c3f0e863e0c000
Kayden Madonna <113037815+kaydenM8654@users.noreply.github.com>
2022-09-11 21:58:21 +0000

    update

---
+++ prog52a.py
@@ -0,0 +1,10 @@
+length = int(input("please enter the length: "))
+width = int(input("please enter the width: "))
+print("the length is", length)
+print("the width is", width)
+
+area = length * width
+perim = 2 * length + 2 * width
+
+print("the area is", area)
+print("the perimeter is", perim)
---
+++ prog54b.py
@@ -0,0 +1,10 @@
+number1 = int(input("please enter number1:"))
+number2 = int(input("please enter number2:"))
+number3 = int(input("please enter number3:"))
+number4 = int(input("please enter number4:"))
+
+sum = number1 + number2 + number3 + number4
+average = sum / 4
+
+print("the sum is", sum)
+print("the average is", average)
