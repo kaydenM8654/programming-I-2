84ce89b72baae40644439968607c99fd6d3175e6
Kayden Madonna <113037815+kaydenM8654@users.noreply.github.com>
2022-09-13 14:38:53 +0000

    update

---
+++ k26.py
@@ -0,0 +1,37 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    beepers_in_bag
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    put_beeper()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
